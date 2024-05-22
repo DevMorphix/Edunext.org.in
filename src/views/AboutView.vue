dec8ae800ff9d2d5318116b95c39eeb274962be9
Badhusha <Badhusha.shaji22@example.com>
2024-05-22 13:13:37 +0530

    WIP about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,40 +1,34 @@
 <template>
+  <navbar />
   <div class="flex flex-col min-h-screen">
- 
-
     <main class="flex-grow container mx-auto px-6 py-8">
-      <div class="flex flex-col lg:flex-row items-center">
-        <div class="lg:w-1/2 mb-8 lg:mb-0">
+      <div class="flex justify-center mb-8">
+        <div class="w-3/4">
           <div class="aspect-w-16 aspect-h-9">
             <video controls class="w-full h-full object-cover rounded-lg">
               <!-- <source src="path/to/video.mp4" type="video/mp4"> -->
             </video>
           </div>
         </div>
-        <div class="lg:w-1/2 lg:ml-8">
-          <p class="text-gray-600 mb-4">
-            Lorem ipsum dolor sit amet consectetur. Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis sagittis
-            scelerisque sit viverra. Nullam est mi commodo amet duis. Proin est euismod enim lobortis pretium erat enim.Lorem
-            ipsum dolor sit amet consectetur. Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis sagittis
-            scalerisque sit viverra. Nullam est mi commodo amet duis. Proin est euismod enim lobortis pretium erat enim.
-          </p>
-          <!-- Add the timeline component here -->
-          <timeline/>
-        </div>
       </div>
-
+      <div class="flex justify-center">
+        <timeline />
+      </div>
       <div class="mt-8">
         <p class="text-gray-600">
-          Lorem ipsum dolor sit amet consectetur. Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis sagittis
-          scelerisque sit viverra. Nullam est mi commodo amet duis. Proin est euismod enim lobortis pretium erat enim. Lorem
-          ipsum dolor sit amet consectetur. Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis sagittis
-          scalerisque sit viverra. Nullam est mi commodo amet duis. Proin est euismod enim lobortis pretium erat enim.Lorem
-          ipsum dolor sit amet consectetur. Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis sagittis
-          scalerisque sit viverra. Nullam est mi commodo amet duis. Proin est euismod enim lobortis pretium erat enim.
+          Lorem ipsum dolor sit amet consectetur. Eget vulputate volutpat
+          condimentum curabitur. Eu ultrices mattis sagittis scelerisque sit
+          viverra. Nullam est mi commodo amet duis. Proin est euismod enim
+          lobortis pretium erat enim. Lorem ipsum dolor sit amet consectetur.
+          Eget vulputate volutpat condimentum curabitur. Eu ultrices mattis
+          sagittis scalerisque sit viverra. Nullam est mi commodo amet duis.
+          Proin est euismod enim lobortis pretium erat enim.Lorem ipsum dolor
+          sit amet consectetur. Eget vulputate volutpat condimentum curabitur.
+          Eu ultrices mattis sagittis scalerisque sit viverra. Nullam est mi
+          commodo amet duis. Proin est euismod enim lobortis pretium erat enim.
         </p>
       </div>
     </main>
-
     <footer class="bg-blue-500 text-white py-4 px-6">
       <div class="container mx-auto flex justify-between items-center">
         <div class="logo">
@@ -77,11 +71,14 @@
 </template>
 
 <script>
-import timeline from '@/components/Timeline.vue'
-export default{
-  name: 'aboud',
+import timeline from "@/components/Timeline.vue";
+import navbar from "@/components/reuse/Navbar.vue";
+// import footer from '@/components/reuse/Footer.vue'
+export default {
+  name: "aboud",
   components: {
-    timeline
-  }
-}
+    timeline,
+    navbar,
+  },
+};
 </script>
