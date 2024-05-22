02f2d97a704d192fc9bf629a899a7b3d694c40ca
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-05-22 15:36:19 +0530

    added home page content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,223 @@
 <template>
-<navbar />
-<div class="bg-white">
-  <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div class="mr-auto place-self-center lg:col-span-7">
-        <h1 class="max-w-2xl mb-4 font-bold md:text-5xl xl:text-6xl dark:text-white">Together we can create a change in our country’s educational arena to bring educational equity</h1>
-        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Do you know India has over 1.2 million primary schools and 5.1 million teachers are working in shaping the future of our country. But are we lacking something behind?</p>
-        <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-          Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-              </a>
-              <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
-              </a> 
+  <navbar />
+  <div class="bg-white">
+    <!-- main section  -->
+    <section class="bg-white dark:bg-gray-900">
+      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <h1 class="max-w-2xl mb-4 font-bold md:text-5xl xl:text-6xl dark:text-white">Together we can create a change
+            in our country’s educational arena to bring educational equity</h1>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Do you
+            know India has over 1.2 million primary schools and 5.1 million teachers are working in shaping the future
+            of our country. But are we lacking something behind?</p>
+          <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
+            <div class="bg-white p-6 rounded-md border shadow-md flex flex-col items-center justify-center">
+              <span class="text-2xl font-bold text-gray-800">100+</span>
+              <h3 class="text-sm font-medium text-gray-500 mb-2">Students engaged with as and learned new things</h3>
             </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-              <img src="/assets/home.svg" alt="mockup">
-            </div>                
+            <div class="bg-white p-6 rounded-md border shadow-md flex flex-col items-center justify-center">
+              <span class="text-2xl font-bold text-gray-800">2+</span>
+              <h3 class="text-sm font-medium text-gray-500 mb-2">Years of experience with students and their activities
+              </h3>
+            </div>
+            <div class="bg-white p-6 rounded-md border shadow-md flex flex-col items-center justify-center">
+              <span class="text-2xl font-bold text-gray-800">50+</span>
+              <h3 class="text-sm font-medium text-gray-500 mb-2">New actives provided to engage students</h3>
+            </div>
           </div>
-        </section>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="/assets/home.svg" alt="mockup">
+        </div>
       </div>
-      
-      <Footer />
+    </section>
+    <!-- our mission -->
+    <section class="bg-blue-100	dark:bg-gray-900">
+      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <h1 class="max-w-2xl mb-4 font-semibold	 md:text-3xl xl:text-4xl dark:text-white">Our Mission</h1>
+          <h1 class="max-w-2xl mb-4 font-bold md:text-5xl xl:text-6xl dark:text-white">Lorem ipsum dolor</h1>
+          <p class="max-w-2xl mb-6 font-normal text-gray-900 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">We
+            believe that quality of education makes somebody a responsible citizens which is a one pitstop solution for
+            overall construction of one’s personality. We’re committed to make a social impact in eliminating
+            educational divide in our country.</p>
+
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="/assets/mission.svg" alt="mockup">
+        </div>
+      </div>
+    </section>
+    <!-- Methodology  -->
+    <section class="bg-white dark:bg-gray-900">
+      <div class="pt-8 pb-0 px-4 mx-auto max-w-screen-xl lg:pt-16 lg:pb-0 lg:px-6 ">
+        <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Methodology</h2>
+        </div>
+        <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+
+          <!-- card 1 -->
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-5">
+              <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#">Continuous Assessment and Personalized Feedback</a>
+              </h3>
+              <span class="text-gray-500 dark:text-gray-400">We believe in a continuous assessment system that ensures
+                every student's progress is monitored closely, allowing us to provide personalized feedback tailored to
+                their needs. Our teachers offer constructive criticism aimed at helping students recognize their
+                strengths and areas for improvement. By prioritizing individual growth over mere academic achievement,
+                we instill in our students the skills and mindset necessary for lifelong learning and success in the
+                ever-evolving 21st-century landscape.</span>
+            </div>
+          </div>
+          <!-- card 2 -->
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-5">
+              <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#">Holistic Development and Community Engagement</a>
+              </h3>
+              <span class="text-gray-500 dark:text-gray-400">We prioritize holistic development, addressing physical,
+                mental, emotional, and social well-being. Our programs promote physical health, stress relief, and
+                emotional resilience through counseling and social skills workshops. Additionally, we encourage
+                community engagement through service projects and volunteering, empowering students to apply their
+                skills to real-world issues and make a positive impact. Through these initiatives, we ensure students
+                receive comprehensive support to excell academically and personally while becoming active contributors
+                to their peer circles.</span>
+            </div>
+          </div>
+          <!-- card 3 -->
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-5">
+              <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#">Life Skills Integration</a>
+              </h3>
+              <span class="text-gray-500 dark:text-gray-400">Edunext prioritizes the integration of a wide range of life
+                skills into the curriculum, recognizing that they encompass various facets of daily living. From
+                practical tasks like cooking and cleaning to essential financial skills such as understanding loans and
+                banking processes, our curriculum aims to equip students with the tools they need to thrive in all
+                aspects of life. By incorporating these skills into our educational framework, we ensure that our
+                students are prepared not only for academic success but also for the practical challenges they will
+                encounter in adulthood. This comprehensive approach empowers students to navigate the complexities of
+                modern life with confidence and competence.</span>
+            </div>
+          </div>
+          <!-- card 4 -->
+
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-5">
+              <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#">Personalized Learning Plans</a>
+              </h3>
+              <span class="text-gray-500 dark:text-gray-400">At Edunext, we understand that every student is unique,
+                with their own strengths, interests, and learning styles. That's why we create individualized learning
+                plans for each student, tailored to their specific needs. Through comprehensive assessments, our
+                teachers identify students' strengths and areas for growth, allowing us to customize instruction and
+                resources accordingly. This personalized approach enables students to progress at their own pace, delve
+                into subjects that captivate their interest, and pursue their passions with enthusiasm. By empowering
+                students to take ownership of their learning journey, we create a sense of autonomy, motivation, and
+                confidence that propels them toward academic excellence and personal fulfillment.</span>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+    <!-- Testimonial -->
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+        <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">From the Students</h2>
+        </div>
+        <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+
+          <!-- card 1 -->
+          <div
+            class="items-center bg-blue-100 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700 relative">
+            <div class="flex flex-col p-5">
+              <span class="text-gray-500 dark:text-gray-400">A good review of the students</span>
+            </div>
+            <div class="absolute bottom-2 right-2">
+              <span class="text-gray-500 dark:text-gray-400">- Author Name</span>
+            </div>
+          </div>
+          <!-- card 2 -->
+          <div
+            class="items-center bg-blue-100 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700 relative">
+            <div class="flex flex-col p-5">
+              <span class="text-gray-500 dark:text-gray-400">A good review of the students</span>
+            </div>
+            <div class="absolute bottom-2 right-2">
+              <span class="text-gray-500 dark:text-gray-400">- Author Name</span>
+            </div>
+          </div>
+          <!-- card 3 -->
+          <div
+            class="items-center bg-blue-100 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700 relative">
+            <div class="flex flex-col p-5">
+              <span class="text-gray-500 dark:text-gray-400">A good review of the students</span>
+            </div>
+            <div class="absolute bottom-2 right-2">
+              <span class="text-gray-500 dark:text-gray-400">- Author Name</span>
+            </div>
+          </div>
+          <!-- card 4 -->
+          <div
+            class="items-center bg-blue-100 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700 relative">
+            <div class="flex flex-col p-5">
+              <span class="text-gray-500 dark:text-gray-400">A good review of the students</span>
+            </div>
+            <div class="absolute bottom-2 right-2">
+              <span class="text-gray-500 dark:text-gray-400">- Author Name</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- supporters -->
+    <h2 class="mb-4 text-3xl font-extrabold text-center text-gray-900 dark:text-white md:text-4xl">Our Supporters</h2>
+    <section class="bg-blue-100 dark:bg-gray-900">
+      <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+        <div class="flex justify-center items-center">
+          <a href="#" class="mx-4">
+            <img src="/assets/Inovuslogo.png" alt="" class="h-20">
+          </a>
+          <a href="#" class="mx-4">
+            <img src="/assets/Dev.x.png" alt="" class="h-20">
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <!-- monthely news letters -->
+
+    <section>
+  <h2 class="my-8 text-3xl font-extrabold text-center text-gray-900 dark:text-white md:text-4xl">
+    Get Monthly News Letter
+  </h2>
+  <div class="my-8 flex justify-center items-center">
+    <div class="flex rounded-full shadow-md bg-white w-auto">
+      <input
+        v-model="searchText"
+        type="text"
+        class="rounded-l-full p-2 pl-4 bg-transparent border-none w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        placeholder="Enter your email"
+      />
+      <button
+        class="rounded-full bg-blue-950 font-bold hover:bg-blue-600 text-white p-2 m-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        Subscribe
+      </button>
+    </div>
+  </div>
+</section>
+
+
+  </div>
+
+  <Footer />
 </template>
 
 <script>
@@ -33,6 +229,6 @@ export default {
   components: {
     navbar,
     Footer
-    }
+  }
 }
 </script>
