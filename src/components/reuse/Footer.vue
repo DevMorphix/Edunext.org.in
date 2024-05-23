5ed15b0c2021169315310eedc8866a3685e53353
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-05-23 23:47:08 +0530

    added footer

--- src/components/reuse/Footer.vue
+++ src/components/reuse/Footer.vue
@@ -1,36 +1,51 @@
 <template>
-<footer class="bg-blue-900 py-8">
-  <div class="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-    <div class="text-white font-bold text-2xl mb-4 md:mb-0"><img src="/assets/Copy Claim.svg" alt=""></div>
-    <nav class="mb-4 md:mb-0">
-      <ul class="flex space-x-4">
-        <li><a href="#" class="text-white hover:text-blue-300">We</a></li>
-        <li><a href="#" class="text-white hover:text-blue-300">Redefine</a></li>
-        <li><a href="#" class="text-white hover:text-blue-300">Learning</a></li>
-        <li><a href="#" class="text-white hover:text-blue-300">Experiences.</a></li>
-      </ul>
-    </nav>
-    <div class="flex space-x-4 mb-4 md:mb-0">
-      <a href="#" class="text-white hover:text-blue-300"><i class="fab fa-linkedin"></i></a>
-      <a href="#" class="text-white hover:text-blue-300"><i class="fab fa-instagram"></i></a>
-      <a href="#" class="text-white hover:text-blue-300"><i class="fab fa-twitter"></i></a>
-      <a href="#" class="text-white hover:text-blue-300"><i class="fab fa-youtube"></i></a>
-    </div>
-    <div class="flex flex-col space-y-2 mb-4 md:mb-0">
-      <div class="flex space-x-4">
-        <a href="#" class="text-white hover:text-blue-300">Terms and Condition</a>
-        <a href="#" class="text-white hover:text-blue-300">Privacy Policy</a>
-        <a href="#" class="text-white hover:text-blue-300">Roles &amp; Responsibilities</a>
+  <footer class="bg-[#0e1525] py-8">
+    <div class="container mx-auto px-4 flex flex-col items-center">
+      <div class="text-white font-bold text-2xl mb-4"><img src="/assets/Copy Claim.svg" alt=""></div>
+      <nav class="flex space-x-4 mb-4">
+        <a href="#" class="text-white hover:text-gray-300">We</a>
+        <a href="#" class="text-white hover:text-gray-300">Redefine</a>
+        <a href="#" class="text-white hover:text-gray-300">Learning</a>
+        <a href="#" class="text-white hover:text-gray-300">Experiences.</a>
+      </nav>
+      <div class="flex space-x-4 mb-4">
+  <a href="#" class="text-white hover:text-gray-300">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+    </svg>
+  </a>
+
+  <a href="#" class="text-white hover:text-gray-300">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162 0 3.403 2.759 6.162 6.162 6.162 3.403 0 6.162-2.759 6.162-6.162 0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+    </svg>
+  </a>
+
+  <a href="#" class="text-white hover:text-gray-300">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.99-8 4.01z" />
+    </svg>
+  </a>
+
+  <a href="#" class="text-white hover:text-gray-300">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124-4.09-.193-7.715-2.157-10.141-5.126-.427.718-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
+    </svg>
+  </a>
+</div>
+      <div class="flex space-x-4 mb-4">
+        <a href="#" class="text-white hover:text-gray-300">Terms and Condition</a>
+        <a href="#" class="text-white hover:text-gray-300">Privacy Policy</a>
+        <a href="#" class="text-white hover:text-gray-300">Roles &amp; Responsibilities</a>
       </div>
+      <hr class="border-gray-700 w-full mb-4" />
+      <div class="text-white">eduneactcommunity@gmail.com</div>
     </div>
-    <div class="text-white">eduneactcommunity@gmail.com</div>
-  </div>
-</footer>
-
+  </footer>
 </template>
-<script>
 
-export default{
-    name: 'Footer',
+<script>
+export default {
+  name: 'Footer',
 }
 </script>
